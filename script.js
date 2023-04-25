acffa11daad11746ab481f11c5c8a2b92111c1a0
Nicolás García <nicolasg.ngl@gmail.com>
2023-04-25 14:56:20 -0500

    proyecto base terminando

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const button1 = document.querySelector('.button1');
+const button2 = document.querySelector('.button2');
 const modal = document.querySelector('.modal');
 const closeButton = document.querySelector('.close');
 
-button1.addEventListener('click',()=>{
+button2.addEventListener('click',()=>{
     modal.classList.remove('hidden');
     modal.classList.add('visible')
 });
